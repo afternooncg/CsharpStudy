8fb3c5e1b9ecfc1b29d5c4b22b7cc35b9c46bd2d
afternooncg <afternooncg@126.com>
2017-06-18 23:30:13 +0800

    MonitorWithTime

--- LittleStuff/CaseStudy/CaseStudy/Program.cs
+++ LittleStuff/CaseStudy/CaseStudy/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CaseStudy.ThreadTest.MonitorTest;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,7 +14,9 @@ namespace CaseStudy
             //Threads
             // CaseStudy.ThreadTest.MonitorTest.MonsterCase.RunTestEnterAndExit();
             //CaseStudy.ThreadTest.MonitorTest.MonitorCase.RunTestWaitAndPluse();
-            CaseStudy.ThreadTest.MonitorTest.MonitorCase.RunTestTryEnter();
+           // CaseStudy.ThreadTest.MonitorTest.MonitorCase.RunTestTryEnter();
+
+            MonitorWithTime.RunTest();
             
         }
     }
---
+++ LittleStuff/CaseStudy/CaseStudy/ThreadTest/MonitorTest/MonitorWithTime.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace CaseStudy.ThreadTest.MonitorTest
+{
+    class MonitorWithTime
+    {
+        static object lockObj = new object();
+
+        static void A()
+        {
+            lock (lockObj) //进入就绪队列 
+            {
+                Thread.Sleep(1000);
+                Monitor.Pulse(lockObj);
+                Monitor.Wait(lockObj); //自我流放到等待队列 
+                Console.WriteLine("A exit...");
+            }
+        }
+
+        static void B()
+        {
+            Thread.Sleep(500);
+            lock (lockObj) //进入就绪队列 
+            {
+                Monitor.Pulse(lockObj);
+            }
+            Console.WriteLine("B exit...");
+        }
+
+        static void C()
+        {
+            Thread.Sleep(800);
+            lock (lockObj) //进入就绪队列 
+            { }
+
+            Console.WriteLine("C exit...");
+        }
+
+        public static void RunTest()
+        {
+            new Thread(A).Start();
+            new Thread(B).Start();
+            new Thread(C).Start();
+            Console.ReadLine();
+        }
+
+    }
+
+}
